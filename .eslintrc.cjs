e8f5a5d6306f2b355defd2587eae00706839f129
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2023-07-31 02:36:22 +0900

    setup eslint & prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
